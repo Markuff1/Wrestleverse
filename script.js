02fb6d64b64c98c9ce350422269f6cc382534397
Markuff1 <marknixon1@outlook.com>
2024-07-02 11:10:23 +0100

    Chagnes to mobile screen as well as changing the background for the mobile version

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function openmenu(){
 function closemenu(){
     sidemeu.style.right = "-150px";
 }
+
+
+
+
+  const scriptURL = "https://script.google.com/macros/s/AKfycbxXyVmGAezX2tXZTWmqsdrsRsNSEJ_sQ6xcbxthjI7405w1Ops_wutQL4t7iwQaGpDR/exec"
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Feedback Submitted"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },3000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
